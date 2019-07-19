09f3e42ce3097e05d99090f6054f93cc67f9c248
bytesalad <52949496+bytesalad@users.noreply.github.com>
2019-07-20 00:18:18 +0200

    Updated Enemy AI
    Now working on instantiating the projectile objects.

---
+++ NL-V_V3/Assets/Scripts/EnemyType1/EnemyType1_InstantiateProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyType1_InstantiateProjectile : MonoBehaviour
+{
+    [SerializeField] private float offsetMagnitude;
+
+    [SerializeField] private GameObject prefab;
+
+    public void RunInstantiate(Vector2 enemyPosition, Vector2 destinationPosition)
+    {
+        Vector2 unitDirVector = (destinationPosition - enemyPosition) / (destinationPosition - enemyPosition).magnitude;
+
+        Vector2 instantiatePosition = offsetMagnitude * unitDirVector;
+
+        Instantiate(prefab, instantiatePosition, Quaternion.identity);//tell the object to run it's velocity/movement script.
+    }
+}
