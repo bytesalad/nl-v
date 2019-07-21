fb2759cbeb08d5e45825911fc2946d0c24cbaef2
bytesalad <52949496+bytesalad@users.noreply.github.com>
2019-07-21 21:33:33 +0200

    Backtrack Commit

--- NL-V_V3/Assets/Scripts/GameManager_Object/GameManager_Startup.cs
+++ NL-V_V3/Assets/Scripts/GameManager_Object/GameManager_Startup.cs
@@ -6,17 +6,12 @@ public class GameManager_Startup : MonoBehaviour
 {
     private GameManager_TextOutput console;
 
-    private string startupString;
+    [SerializeField] private TextAsset textFile;
 
     private void Start()
     {
-        startupString = ">> NL-V Startup" + "\n" +
-            ">> This program is set to load automatically on first time boot" + "\n" +
-            ">> Name: First Run Calibration" + "\n" +
-            ">> Neuro-Link - Visual Version 1.0.165" + "\n" +
-            ">> Use the mouse_construct and point_click_method() to shoot the data_file.";
         console = gameObject.GetComponent<GameManager_TextOutput>();
         console.ClearConsole();
-        console.OutputToConsole(startupString);
+        console.OutputToConsole(textFile.text);
     }
 }
